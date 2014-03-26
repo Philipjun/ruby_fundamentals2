b348450436363cc958ea5fbbbb8574a33f37a5b8
Philip Jun <inbox@philipjun.com>
2014-03-26 15:13:42 -0400

    student list

--- exercise7.rb
+++ exercise7.rb
@@ -1,6 +1,20 @@
+
+
+
 students = {
   :cohort1 => 34,
-  :cohort2 => 42,
-  :cohort3 => 22
+  :cohort3 => 22,
+  :cohort4 => 43,
 }
+
+
+students.each  do |key, value|
+puts "#{key}: #{value * 1.05}"
+
+end
+
+sum = 0
+
+
+
 
